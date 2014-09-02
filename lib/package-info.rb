ea418b41480632b566aedb416826c7a31fe94257
Angelos Kapsimanis <angelos@advancedtelematic.com>
2014-09-02 18:09:28 +0200

    Added parsing for unstructured copyright documents

--- lib/package-info.rb
+++ lib/package-info.rb
@@ -3,7 +3,20 @@ require 'csv'
 
 class PackageInfo
   COPYRIGHT_FIELDS = ["Format", "Upstream-Name", "Upstream-Contact",
-    "Source", "Disclaimer", "Comment", "License", "Copyright", "Files"]
+                      "Source", "Disclaimer", "Comment", "License", 
+                      "Copyright", "Files"]
+
+  LICENSE_TAGS = ["gpl", "mit", "license", "license"]
+
+  COPYRIGHT_TAGS = ["copyright"]
+
+  UPSTREAM_NAME_TAGS = ["author", "developer", "upstream"]
+
+  FILES_TAGS = ["files"]
+
+  DISCLAIMER_TAGS = ["disclaimer"]
+
+  FORMAT_TAGS = ["format"]
 
   def create_package_file filename = "packages.csv"
     get_package_list.each do |package|
@@ -46,9 +59,31 @@ class PackageInfo
 
     lines.each do |line|
       field_name, field_content = parse_dep_5_line(line, field_name, field_content)
-
       if COPYRIGHT_FIELDS.include?(field_name)
         fields[field_name] = field_content
+      end
+    end
+
+    fields
+  end
+
+  def parse_non_dep_5_compatible text
+    fields, paragraphs = init_fields_hash, text.split(/^$\n/)
+
+    paragraphs.each do |paragraph|
+      case
+      when match_tags(LICENSE_TAGS, paragraph)
+        fields["License"] += paragraph
+      when match_tags(COPYRIGHT_TAGS, paragraph)
+        fields["Copyright"] += paragraph
+      when match_tags(UPSTREAM_NAME_TAGS, paragraph)
+        fields["Upstream-Name"] += paragraph
+      when match_tags(FILES_TAGS, paragraph)
+        fields["Files"] += paragraph
+      when match_tags(DISCLAIMER_TAGS, paragraph)
+        fields["Disclaimer"] += paragraph
+      when match_tags(FORMAT_TAGS, paragraph)
+        fields["Format"] += paragraph        
       end
     end
     fields
@@ -63,14 +98,15 @@ class PackageInfo
     return field_name, field_content
   end
 
-  def parse_non_dep_5_compatible text
-    fields, lines = {}, text.split(/^$\n/)
-    field_name, field_content = "", ""
+  def init_fields_hash
+    fields = {}
+    COPYRIGHT_FIELDS.each {|f| fields[f] = ""}
+    fields
+  end
 
-    puts lines.length
-    lines.each {|line| puts "!!!!!!!!!!!!!!!\n #{line}" }
-
-    {"foo" => "bar"}    
+  def match_tags tags, paragraph
+    tags.each {|tag| return true if paragraph.downcase.include?(tag)}
+    false
   end
 
 end
--- test/test.rb
+++ test/test.rb
@@ -3,5 +3,5 @@ require 'package-info'
 
 pi = PackageInfo.new
 
-package = pi.parse_copyright_file("cassandra")
+package = pi.parse_copyright_file("python")
 package.each {|k,v| puts "#{k} ----- #{v}"}
