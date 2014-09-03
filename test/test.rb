2c215bf8a9d4f93aad05aa3811142e02e055d4e6
Angelos Kapsimanis <angelos@advancedtelematic.com>
2014-09-03 11:08:40 +0200

    Added main test case in test.rb

--- test/test.rb
+++ test/test.rb
@@ -1,7 +1,4 @@
 # encoding: utf-8
 require 'package-info'
 
-pi = PackageInfo.new
-
-package = pi.parse_copyright_file("python")
-package.each {|k,v| puts "#{k} ----- #{v}"}
+PackageInfo.new.create_package_file
